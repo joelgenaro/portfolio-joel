e2d751b5b5957ed162a9b2260fe05b6582e41a1f
Joel Genaro <joel.ggdev@gmail.com>
2024-12-05 01:37:52 +0900

    import { NextSeo } from "next-seo";

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,4 @@
 import { NextSeo } from "next-seo";
-
 import ProjectCard from "@/components/projects/project-card";
 import { PROJECTS_CARD } from "@/data/projects";
 import { siteMetadata } from "@/data/siteMetaData.mjs";
